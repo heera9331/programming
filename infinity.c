37e46fdc93392e94de9a11303814e2cd73a00968
heera9331 <71516269+heera9331@users.noreply.github.com>
2021-06-21 22:58:44 +0530

    Create infinity.c

---
+++ infinity.c
@@ -0,0 +1,470 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//    // int a,b,c;
+//    // printf("Enter the two number :  ");
+//    // scanf("%d %d",&a,&b);
+//    // c = a+b;
+//    // printf("%d",c);
+
+//    // int a,b;
+//    // scanf("%d %d",&a,&b);
+//    // printf("%d %d",a,b);
+
+//    // int a,b,c;
+//    // printf("Enter two positive integer :");
+//    // scanf("%d %d",&a,&b);
+//    // c = a+b;
+//    // printf("%d",c);
+
+//    // Program to create a simple calculator
+//    // Program to create a simple calculator
+
+//    // {
+//    //    char operator;
+//    //    double n1, n2;
+
+//    //    printf("Enter an operator (+, -, *, /): ");
+//    //    scanf("%c", &operator);
+//    //    printf("Enter two operands: ");
+//    //    scanf("%lf %lf", &n1, &n2);
+
+//    //    switch (operator)
+//    //    {
+//    //    case '+':
+//    //       printf("%.1lf + %.1lf = %.1lf", n1, n2, n1 + n2);
+//    //       break;
+
+//    //    case '-':
+//    //       printf("%.1lf - %.1lf = %.1lf", n1, n2, n1 - n2);
+//    //       break;
+
+//    //    case '*':
+//    //       printf("%.1lf * %.1lf = %.1lf", n1, n2, n1 * n2);
+//    //       break;
+
+//    //    case '/':
+//    //       printf("%.1lf / %.1lf = %.1lf", n1, n2, n1 / n2);
+//    //       break;
+
+//    //    // operator doesn't match any case constant +, -, *, /
+//    //    default:
+//    //       printf("Error! operator is not correct");
+//    //    }
+
+//    //    // return 0;
+//    // }
+
+//    // char UserInput;
+//    // float Num1,Num2;
+//    // printf("Choose operation : +, - , *, / ");
+//    // scanf("%c",&UserInput);
+//    // printf("Enter the two number : ");
+//    // scanf("%f %f",&Num1,&Num2);
+//    // switch (UserInput)
+//    // {
+//    // case '+':
+//    //    printf("%f + %f = %f",Num1,Num2,Num1+Num2);
+//    //    break;
+
+//    // case '-':
+//    //    printf("%f - %f = %f",Num1,Num2,Num1-Num2);
+//    //    break;
+
+//    // case '*':
+//    //    printf("%f * %f = %f",Num1,Num2,Num1*Num2);
+//    //    break;
+
+//    // case '/':
+//    //    printf("%f + %f = %f",Num1,Num2,Num1/Num2);
+//    //    break;
+
+//    // default:
+//    //    printf("Please Enter the valid input !!!!!");
+// // }
+
+// #include <stdio.h>
+// #include <conio.h>
+
+// {
+//    // write to programm - convert length unit kilometer to meter
+//    // float km,m;
+//    // printf("Enter kilometer : ");
+//    // scanf("%f",&m);
+//    // km = m*1000;
+//    // printf("%.0f kilometer = %.2f meter",m,km);
+
+//    // write programm - convert unit meter to centimeter
+//    // float cm,m;
+//    // printf("Enter meter : ");
+//    // scanf("%f",&m);
+//    // cm = m*100;
+//    // printf("%.0f meter = %.2f centimeter",m,cm);
+
+//    // change temperature unit farenhite to celius
+// #include <stdio.h>
+// void main()
+// {
+//    float f,c;
+//    printf("Enter Farenhite  : ");
+//    scanf("%f",&f);
+//    c = (f-32)/9*5;
+//    printf("%2.f F = %.2f Celcius",f,c);
+
+// }
+// #include <stdio.h>
+// void main()
+// {
+//    float f,c;
+//    printf("Enter Farenhite : ");
+//    scanf("%f",&f);
+//    c = (f-32)/9*5;
+//    printf("%2.f",c);
+// }
+
+// #include <stdio.h>
+
+// void main()
+// {
+//    int n,sum;
+//    scanf("%d",&n);
+//    sum = n*(n+1)*(n+2)*6;
+//    printf("%d",sum);
+// }
+
+// #include <stdio.h>
+// #include <conio.h>
+
+// void main()
+
+// {
+//    int a,n,d,tn;
+//    printf("Enter the value of a  d");
+//    scanf("%d %d %d",&a,&n,&d);
+//    tn = a+(n-1)*d;
+//    printf("%d",tn);
+
+// }
+
+// #include <stdio.h>
+// #include <conio.h>
+
+// void main()
+// {
+//    int n,a,d,sum;
+//    printf("Enter the value of n.a,d");
+//    scanf("%d %d %d",&n,&a,&d);
+//    sum = a-(n-1)*d;
+//    printf("%d",sum);
+// }
+
+// #include <stdio.h>
+
+// void main()
+// {
+//    int n,a,d,sum;
+//    printf("Enter value of n,a,d");
+//    scanf
+// }
+
+// #include <stdio.h>
+// #include <conio.h>
+
+// void main()
+// {
+//    float t,r,p,result;
+//    printf("Enter the value of t,r,p");
+//    scanf("%f %f %f",&t,&r,&p);
+//    // result = (t*r*p)/100;
+//    result = (t*r*p/100);
+//    printf("%.2f",result);
+
+// }
+
+// #include <stdio.h>
+// #include <conio.h>
+
+// void main()
+
+// {
+// int a,b;
+// scanf("%d",&a);
+// printf("%d",a);
+// int a,b;
+// scanf("%d",&a);
+// b = a*a-a;
+// printf("%d",b);
+
+// int n,m;
+// scanf("%d",&n);
+// m = n*(n+1)/2;
+// printf("%d",m);
+
+//
+// int tn,a,n,d;
+// printf("Enter the value of a,n,d : ");
+// scanf("%d %d %d",&a,&n,&d);
+// tn = a+(n-1)*d;
+// printf("%d",tn);
+
+// int a,d,n,sn;
+// a=7,d=7;
+// printf("Enter the value of : n :");
+// scanf("%d",&n);
+// sn = (2*a+(n-1)*d)*n/2;
+// printf("%d",sn);
+
+// int a,b;
+// printf("Enter a number; : ");
+// scanf("%d",&a);
+// b = a*a*a;
+// printf("%d",b);
+
+// }
+
+// b*b - 4*a*c = find root
+// #include <stdio.h>
+// #include <conio.h>
+// void main()
+// {
+//    int root,a,b,c;
+//    printf("enter the value of a , b and c : ");
+//    scanf("%d %d %d",&a,&b,&c);
+//    root = b*b - 4*a*c;
+//    printf("%d",root);
+//    // return 0;
+
+//    getch();
+//    // clrscr();
+
+// }
+
+// #include <stdio.h>
+
+// void main()
+// {
+//    int s, t, a, u;
+//    printf("Enter the value of d t, a,u");
+//    scanf("%d %d %d", &t, &a, &u);
+// s = u * t + (a * t * t) / 2;
+//    printf("%d", s);
+// }
+
+// #include <stdio.h>
+// void main()
+// {
+//    // int force, m,a;
+//    // printf("enter the value of m,a : \n");
+//    // scanf("%d %d ",&m,&a);
+//    // force = m*a;
+//    // printf("%d N",force);
+
+//    // int w,f,d;
+//    // printf("Enter the value of : f,d");
+//    // scanf("%d %d",&f,&d);
+//    // w = f*d;
+//    // printf("%d joule",w);
+
+// }
+// // comments - comments not for compiler
+// #include <stdio.h>
+// void main()
+// {
+//    int work,force,acceration,displacement;
+//    printf("Enter the val")
+
+// }
+
+// }
+
+// #include <stdio.h>
+// void main()
+// {
+//    int n,m;
+//    printf("Enter a number : ");
+//    scanf("%d",&n);
+//    m = n*(n+1)/2;
+//    printf("%d",m);
+// }ettt mdmwnkweru9
+
+// Number guessing Game
+
+// #include <stdio.h>
+// void main()
+// {
+//       printf("Hello world");
+// }
+
+// #include <stdio.h>
+// #include <conio.h>
+// void main()
+// {
+//       int km, m;
+//       printf("Please enter kilometer to convert meter : ");
+//       scanf("%i", &km);
+//       m = km * 1000;
+//       printf("%ikm = %im", km, m);
+// }
+
+// find the area of triangle using formula
+// #include <stdio.h>
+// #include <conio.h>
+
+// void main()
+// {
+//       int l, w;
+//       printf("Please enter the height and width : ");
+//       scanf("%i %i", &l, &w);
+//       float area;
+//       area = 0.5 * l * w;
+//       printf("area if triangle is 1/2 * %i * %i = %.2f",l,w,area);
+// }
+
+// // octal to decimal convertion
+// #include <stdio.h>
+// #include <math.h>
+
+// int main()
+// {
+//       long int o, d = 0;
+//       int i = 0 ;
+//       printf("Enter the octal number : ");
+//       scanf("%ld", &o);
+//       while (o != 0)
+//       {
+//             d = d + (o % 10) * pow(8, i++);
+//             o = o / 10;
+//       }
+//       printf("decimal value %ld", d);
+// }
+
+// sum of n numbers
+// #include <stdio.h>
+
+// void main()
+// {
+//       int n, m;
+//       printf("Enter the number which you find sum : ");
+//       scanf("%d", &n);
+//       m = n * (n + 1) / 2;
+//       printf("the sum of numbers is %i = %i", n, m);
+// }
+
+// octal to hexaadecimal convertion
+// #include <stdio.h>
+// #include <math.h>
+// #include <string.h>
+
+// int main()
+// {
+//       int octaltobinary = {0,1,10,11,100,101,110,111};
+//       char hexadecimal = [10];
+//       char hex[10];
+//       log int binary = 0;
+//       int octal;
+//       int rem = 0;
+//       int position = 1;
+//       int len = 0 ;
+//       int k = 0;
+//       printf("Enter a octal number :  ");
+//       scanf("%d",&octal);
+//       while (octal != 0)
+//       {
+//             rem = octal % 10;
+//             binary = octaltobinary[rem]*position+binary;
+//             octal = octal /10;
+//             position = position * 1000;
+
+//       }
+//       printf("The binary number is : %ld",binary);
+
+// }
+
+// check even or odd number
+// #include <stdio.h>
+
+// int main()
+// {
+//       int n;
+//       printf("Enter a number : ");
+//       scanf("%i",&n);
+//       if (n%2==0) {
+//             printf("Given number %i is even ",n);
+//       } else {
+//             printf("Given number %i is odd ",n);
+//       };
+// }
+
+// given year check leap year of not
+// #include <stdio.h>
+
+// int main()
+// {
+//       int n;
+//       printf("Enter a number : ");
+//       scanf("%i",&n);
+//       if (n%4==0) {
+//             printf("given year %i is leap year ",n);
+//       } else {
+//             printf("given year %i is not leap year ",n);
+//       };
+// }
+
+
+// find the sum of n^2 natural number 
+// #include <stdio.h>
+
+// int main()
+// {
+//     int n, m;
+//     printf("Enter a number which you find sum of n square number : ");
+//     scanf("%i", &n);
+//     m = (n * (n + 1) * (2 * n + 1)) / 6;
+//     printf("%i", m);
+// }
+
+// -----------------------------------------------------------------------------------------------------
+
+// #include <stdio.h>
+// #include <conio.h>
+
+// int main()
+// {
+// 	int x = 10, y = 15, temp;
+// 	temp = x;
+// 	x = y;
+// 	y = temp;
+// 	printf("%d %d", x, y);
+// 	return 0;
+
+// }
+
+// find square to the user given number 
+// #include <stdio.h>
+// #include <conio.h>
+
+// int main()
+// {
+// 	int a, square;
+// 	printf("Enter a number which you find square : ");
+// 	scanf("%d",&a);
+// 	square = a*a;
+// 	printf("Square of a is  : = %d",square);
+// 	return 0;
+// }
+
+
+
+
+#include <stdio.h>  
+int main()  
+{  
+    int a=2 , b;  
+    int b=2/0;  
+    printf("The value of b is : %d", b);  
+    return 0;  
+}  
+
+
+
